3f7f287064febcda192abdc25454a520016eb35c
shaunamooney <73603731+shaunamooney@users.noreply.github.com>
2023-04-06 21:08:35 +0100

    found the survey data problem

--- R/contraceptive_use_import.R
+++ R/contraceptive_use_import.R
@@ -2,14 +2,15 @@
 #'
 #' @inheritParams fit_fp_c
 contraceptive_use_import <- function(is_in_union, surveydata_filepath, division_numeric_code, subnational) {
+  
   if (!is.null(surveydata_filepath)) {
-
+    
     contraceptive_use <- readr::read_csv(surveydata_filepath)
     format_check(fpemlocal::contraceptive_use_format, contraceptive_use)
     
     if (is_in_union %in% c("Y","N")){ # SM 13082021
       contraceptive_use <- contraceptive_use %>%
-      dplyr::filter(is_in_union == !!is_in_union)
+        dplyr::filter(is_in_union == !!is_in_union)
     }
     contraceptive_use <- contraceptive_use %>%
       dplyr::filter(age_range == "15-49")  %>%
@@ -20,9 +21,10 @@ contraceptive_use_import <- function(is_in_union, surveydata_filepath, division_
   } else {
     
     if (is_in_union %in% c("Y","N")){ # SM 13082021
-      contraceptive_use <- fpemlocal::contraceptive_use %>% dplyr::filter(is_in_union == !!is_in_union)
+      contraceptive_use <- fpemlocal::contraceptive_use %>%
+        dplyr::filter(is_in_union == !!is_in_union)
     }
-    contraceptive_use <- fpemlocal::contraceptive_use %>%
+    contraceptive_use <- contraceptive_use %>%
       dplyr::filter(age_range == "15-49")  %>%
       dplyr::filter(division_numeric_code == !!division_numeric_code) %>%
       dplyr::mutate(ref_date = floor((start_date + end_date) / 2))
