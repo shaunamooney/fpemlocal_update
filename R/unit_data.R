feabcf1ef0ba9161bc36bed2758aaa36b7bb1e27
shaunamooney <73603731+shaunamooney@users.noreply.github.com>
2023-04-06 09:15:07 +0100

    trying to find divisions error

--- R/unit_data.R
+++ R/unit_data.R
@@ -1,6 +1,6 @@
 
 unit_data <- function(division_numeric_code, is_in_union) {
-  divisions %>%
+  fpemlocal::divisions %>%
     dplyr::filter(!is.na(sub_region_numeric_code)) %>% 
     dplyr::mutate(is_developed_region = factor(is_developed_region, levels = c("Y", "N"))) %>%
     dplyr::filter(division_numeric_code == !!division_numeric_code) 
