80ea11a522bda1261fd52600b5f6ba02fd6d3ccb
shaunamooney <73603731+shaunamooney@users.noreply.github.com>
2024-01-23 11:22:07 +0000

    Update list_service_stats.R

--- R/list_service_stats.R
+++ R/list_service_stats.R
@@ -1,13 +1,9 @@
-
-#' list_service_stats
+#' list_service_stats - no fixed SE estimates used, user inputted only
 #'
 #' @inheritParams fit_fp_c
 #' @param service_stats_filepath File path to service stat csv
 #' @param model_seq_years Years of model estimation from\code{\link{year_sequence_maker}}.
 #'
-#' run for most recent emu database
-#' check that this is done by observation not by database type - look at diff countries
-#' is it just when you use more do they have more impact - check for multiple countries 
 #' 
 list_service_stats <- function(
     service_stats_filepath,
@@ -17,69 +13,51 @@ list_service_stats <- function(
 )
   
 {
-  # ******2021 database AW source specific standard error term*****************
-  #se_visits = 0.0373
-  #se_clients = 0.0354 
-  #se_facilities = 0.0568
-  #se_users = 0.0835
-
   
-  if (!is.null(service_stats_filepath)) {
-    if (is.na(first_year_observed)) {
-      stop("No available contraceptive use data for this run. Service statistics cannot be used.")
-    }
-    if (!is.null(service_stats_filepath)) {
-      ss <- readr::read_csv(service_stats_filepath)
-    }
-    # else {
-    #   ss <- fpemservicestat::service_stats
-    # }
-    
-    
-    format_check(fpemlocal::service_stats_format, ss)
-    ss <- ss %>% dplyr::arrange(year)
-    ss <- ss %>%
-      dplyr::filter(division_numeric_code == !! division_numeric_code) %>%
-      dplyr::filter(year >= first_year_observed) %>%
-      dplyr::group_by(ss_type) # %>% # allows delta to calculate properly when multiple emus used
-    # tidyr::drop_na() 
+  if (is.null(service_stats_filepath)) {
+    return(NULL)
+  }
+  
+  ss <- readr::read_csv(service_stats_filepath)
+  format_check(fpemlocal::service_stats_format, ss)
+  
+  
+  ss <- ss %>%
+    dplyr::arrange(year) %>%
+    dplyr::filter(division_numeric_code == !! division_numeric_code) %>%
+    dplyr::filter(year >= first_year_observed) %>%
+    dplyr::group_by(ss_type)
     
     if(!("delta_emu" %in% names(ss))){
       ss <- ss %>%
-        dplyr::mutate(ss_delta = c(NA,diff(emu))) %>% # have to fix this 
+        dplyr::mutate(ss_delta = c(NA,diff(emu))) %>% 
         dplyr::mutate(ss_year_lag = dplyr::lag(year)) 
     }
     
     else{
       ss <- ss %>% 
-        dplyr::mutate(ss_delta = delta_emu) %>% # have to fix this 
+        dplyr::mutate(ss_delta = delta_emu) %>% 
         dplyr::mutate(ss_year_lag = year) 
     }
     
-    #if (!("se" %in% names(ss))) {
-     # ss <- ss %>%
-    #    dplyr::mutate(ss_se = ifelse(ss_type == "visits", se_visits,
-    #                                 ifelse(ss_type == "clients", se_clients,
-   #                                         ifelse(ss_type == "facilities", se_facilities,  se_users))))
-    #}
-    
-    #else {
-      ss <- ss %>%
-        dplyr::mutate(ss_se = se)
-   # }
+    # pull in user inputted se estimates
+    ss <- ss %>%
+      dplyr::mutate(ss_se = se)
+      
     if (nrow(ss) == 0) {
       stop("No available service stats after filtering. Service stats were filtered based on division numeric code.")
     }
     
     pop_type <- ss %>% dplyr::pull(pop_type) %>% unique()
     
-    if(pop_type == "Y"){
-      pop_index <- 1 # inform married women mcpr
-    } else{
-      pop_index <- 3 # inform all women mcpr
+    if (length(pop_type) > 1) {
+      stop("Multiple population types found. Unable to determine pop_index.")
     }
     
+    pop_index <- ifelse(pop_type == "Y", 1, 3) # informing married women or all women mcpr
+    
     k_index <- ss$ss_delta %>% is.na %>% `!` %>% which
+    
     list_ss_data <- list(K = k_index %>% length,
                          get_t_k = match(x = floor(ss$year),model_seq_years), # k+1 years and k differences
                          ss_delta_k = ss$ss_delta[k_index],
